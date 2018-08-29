a6769a94a90577fea13e8d99b7becb587221ed9f
tobiasgronberg <42805169+tobiasgronberg@users.noreply.github.com>
2018-08-29 14:39:36 +0200

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,57 @@
+import random
+import os
+import sys
+
+"""x = 30
+if (x == 30):
+    print('\nstämmer')
+elif (x > 30):
+    print('\nstämmer ej')
+elif (x < 30):
+    print('\nstämmer ej') """
+
+"""x = 100
+if x > 99:
+    print('ditt köp är godkänt')
+elif x < 100:
+    print('ditt saldo är för litet')"""
+
+"""x = 100
+
+
+if x >= 100:
+    print('du har fått VG')
+elif x >= 50:
+    print('grattis du klarade provet')
+else:
+    print('du har fått icke godkänt')"""
+
+"""x = 1000
+
+if x != 1000:
+    print('du sköt för högt eller för lågt')
+elif x == 1000:
+    print('du prickade rätt, grattis!')"""
+
+"""x = random.randrange(0, 101)
+
+
+print(x)
+
+while x != 20:
+    y += 1
+    x = random.randrange(0, 101)
+    print("nummer", x,"efter", y, "slumpade försök")"""
+
+while random.randrange(0, 101)!= 20:
+
+
+
+
+
+
+
+
+
+
+
